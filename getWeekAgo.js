492ff36b49350d8e921473d0a0aea1393b2b0916
Grzegorz Zapała <zapala.grzegorz@gmail.com>
2019-03-14 12:09:48 +0100

    Update getWeekAgo.js

--- getWeekAgo.js
+++ getWeekAgo.js
@@ -1,17 +1,31 @@
-    /**
-     * 
-     * @param {today} date 
-     * @returns string of a day weeek formatted: year-week-day of a day weeke
-     */
-    function getWeekAgo() {
-        const today = new Date();
-   
-        const weekAgo =  new Date(today.setDate(today.getDate() - 7));
-    
-        const weekAgoDay = weekAgo.getDate() < 10 ? '0'+weekAgo.getDate() : weekAgo.getDate();
-        const weekAgoMonth = weekAgo.getMonth() < 10 ? '0'+ (weekAgo.getMonth()+1) : weekAgo.getMonth();
-        const weekAgoYear = weekAgo.getFullYear();
-    
-        return `${weekAgoYear}-${weekAgoMonth}-${weekAgoDay}`;
+/* eslint-disable no-unused-vars */
 
+/**
+ * Podaj datę na wyznaczoną liczby dni wstecz
+ * @param {number} daysBefore - liczba dni wstecz, która ma być punktem wyjścia, domyślnie 7
+ * @param {string} format - format wyjściowy. Domyślnie 'dd.mm.yyyy'. Opcja narazie: 'yyyy-mm-dd'
+ * @returns string - domyślnie week-day of a day week
+ */
+function getDateDaysAgo (daysBefore = 7, format) {
+    
+    const dateOptions = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    };
+
+    const today = new Date();
+
+    const dayAgo = new Date(today.setDate(today.getDate() - daysBefore));
+
+    const date = dayAgo.toLocaleString(navigator.language, dateOptions);
+
+    if (format === 'yyyy-mm-dd') {
+
+        /* date jest w formacie 'dd.mm.yyyy' -> 'yyyy-mm-dd' */
+        return date.replace(/(\w+)[.](\w+)[.](\w+)/g, '$3-$2-$1');
     }
+    else {
+        return date;
+    }
+}
